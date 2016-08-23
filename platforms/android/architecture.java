dda70786c4af54ea7166882f3cbb65639ac5a90b
usman-coe <usman2535@gmail.com>
2016-08-23 15:13:30 +0700

    test fix whit groovy

--- platforms/android/architecture.java
+++ platforms/android/architecture.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 
 public class FilenotFound_Demo {
-    
+
    public static void main(String args[]){		
       File file=new File("E://file.txt");
       FileReader fr = new FileReader(file); 
