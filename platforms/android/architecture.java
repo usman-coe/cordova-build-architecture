c08ad0b499642109320cb22892a0e0f474464fa6
usman-coe <usman2535@gmail.com>
2016-08-24 13:31:54 +0700

    change java code

--- platforms/android/architecture.java
+++ platforms/android/architecture.java
@@ -1,1 +1,8 @@
-ext.cdvBuildArch = 'x86';
+public class buildArchTest {
+
+   public static void main(String args[]){		
+    //   ext.cdvBuildArch = 'x86';
+      ext.put("cdvBuildArch", "x86");
+   }
+   
+}
